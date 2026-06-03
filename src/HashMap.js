5b6084092ce710cd963ad52a8aa9852a28da770f
Appropriate32 <frostghost32@gmail.com>
2026-06-03 19:03:13 +0500

    hashing function implemented

--- src/HashMap.js
+++ src/HashMap.js
@@ -5,4 +5,15 @@ class HashMap {
     this.loadFactor = 0.75;
     this.capacity = 16;
   }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
+  }
 }
